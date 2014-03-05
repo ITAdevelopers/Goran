d6fbf07ecec271d3ebb30600deb727deaadfa156
GoranK <goran.krajinovic@gmail.com>
2014-03-05 19:04:24 +0100

    smiješni layout i procesiranje upisa korisnika u bazu

---
+++ form.php
@@ -0,0 +1,35 @@
+﻿
+<?php
+
+/* OVO JE PRVA VARIJANTA POVEZIVANJA SA BAZOM KOJU SAM KOMENTARISAO NAKON ŠTO SAM PROMIJENIO NA PDO 
+
+$con = mysqli_connect("localhost", "root", "", "itadev");
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+  else
+  
+  $sql="insert into korisnici (ID, ime, lozinka, status) values (null, '$_POST[username]', '$_POST[password]', 2)";
+  if (!mysqli_query($con,$sql))
+  {
+  die('Error: ' . mysqli_error($con));
+  }
+echo "dodao";
+ */
+/* $sql= "select * from korisnici";
+$res = mysqli_query($con, $sql);
+while ($red = mysqli_fetch_assoc($res)) {
+
+echo $red['ime'];
+echo '<br />';
+}
+mysqli_close($con);?>
+ */
+
+ 
+ $pdo = new PDO("mysql:host=localhost;dbname=itadev", "root", ""); 
+ $pdo->exec("insert into korisnici (ID, ime, lozinka, status) values (null, '$_POST[username]', '$_POST[password]', 2)");
+ $pdo = null;
+ 
+ header('Location: http://localhost/cms/index.php');
---
+++ index.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<script type="text/javascript" src="http://code.jquery.com/jquery-latest.pack.js"></script>
+<script>
+
+$(document).ready(function() {	
+
+	//select all the a tag with name equal to modal
+	$('a[name=modal]').click(function(e) {
+		//Cancel the link behavior
+		e.preventDefault();
+		
+		//Get the A tag
+		var id = $(this).attr('href');
+	
+		//Get the screen height and width
+		var maskHeight = $(document).height();
+		var maskWidth = $(window).width();
+	
+		//Set heigth and width to mask to fill up the whole screen
+		$('#mask').css({'width':maskWidth,'height':maskHeight});
+		
+		//transition effect		
+		$('#mask').fadeIn(1000);	
+		$('#mask').fadeTo("slow",0.8);	
+	
+		//Get the window height and width
+		var winH = $(window).height();
+		var winW = $(window).width();
+              
+		//Set the popup window to center
+		$(id).css('top',  winH/2-$(id).height()/2);
+		$(id).css('left', winW/2-$(id).width()/2);
+	
+		//transition effect
+		$(id).fadeIn(2000); 
+	
+	});
+	
+	//if close button is clicked
+	$('.window .close').click(function (e) {
+		//Cancel the link behavior
+		e.preventDefault();
+		
+		$('#mask').hide();
+		$('.window').hide();
+	});		
+	
+	//if mask is clicked
+	$('#mask').click(function () {
+		$(this).hide();
+		$('.window').hide();
+	});			
+
+	$(window).resize(function () {
+	 
+ 		var box = $('#boxes .window');
+ 
+        //Get the screen height and width
+        var maskHeight = $(document).height();
+        var maskWidth = $(window).width();
+      
+        //Set height and width to mask to fill up the whole screen
+        $('#mask').css({'width':maskWidth,'height':maskHeight});
+               
+        //Get the window height and width
+        var winH = $(window).height();
+        var winW = $(window).width();
+
+        //Set the popup window to center
+        box.css('top',  winH/2 - box.height()/2);
+        box.css('left', winW/2 - box.width()/2);
+	 
+	});
+	
+});
+
+</script>
+<title>ITADEV</title>
+<link href="layout.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+<div id="boxes">
+<div id="wrapp">
+<div id="header">ovo je header</div>
+<div id="leftbox"><a href="#dialog1" name="modal">Prijava</a></div>
+
+<div id="rightbox">Content for  id "rightbox" Goes Here</div>
+<div id="main">Content for  id "main" Goes Here</div>
+</div>
+
+<div id="dialog1" class="window">
+  <div class="d-header"><form action="form.php" method="post">
+    <input type="text" value="username" name="username" onclick="this.value=''"/><br/>
+    <input type="password" value="Password" name="password" onclick="this.value=''"/><br/>
+<input type="submit" /></form>
+  </div>
+  <div class="d-blank"></div>
+</div>
+<div id="mask"></div>
+</div>
+
+</body>
+</html>
