f15267d01cc268fbcccdf695815ea0b22c3bee40
Mishdish <lakyller@gmail.com>
2014-03-05 19:17:31 +0100

    test ?proba
    probujem upload

---
+++ vezba1.php
@@ -0,0 +1,61 @@
+﻿
+<?php
+    require 'funkcija.php';
+
+	$lokacije = array (
+		"skopje"=>"makedonija",
+		"beograd"=>"srbija",
+		"atina"=>"grcka",
+		"madrid"=>"spanija",
+		);
+	
+?>
+
+<br />
+<br />
+<br />
+<br />
+<hr />
+
+<form>
+<?php
+
+	kontrola("text","text");
+	kontrola("submit","unosi");
+?>
+</form>
+
+
+<?php
+function kontrola ($typ,$val){
+	
+	echo "<input type='{$typ}' value ='{$val}' \>";		
+}
+
+/*
+$stringce="Pristup pisanja uslovnih blokova bez vitičastih zagrada sretaćete i kod drugih struktura za kontrolu toka, ali ne i u funkcijama ili klasama";
+echo str_word_count($stringce);
+
+/*
+$lipsum = "Lorem Ipsum is simply dummy text of the printing and typesetting industry.";
+$lipsumArr= explode(" ",$lipsum);
+$novaNiza="";
+for($i=0;$i<sizeof($lipsumArr);$i++){
+	if(strlen($novaNiza." ")+strlen($lipsumArr[$i])<15)
+		 $novaNiza.= "".$lipsumArr[$i];
+
+	else {
+		if($novaNiza!=""){
+			echo $novaNiza."<br />";
+		}
+		$novaNiza=$lipsumArr[$i];
+	}
+		
+		}
+echo $novaNiza;
+
+*/
+
+echo "<input name=\"\" type=\"text\" value=\"proba\" \>";
+?>
+
